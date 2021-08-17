0d7b0c88ce58b2e649b31228b9e77717667fb3df
Omar-Ess <souafomar@gmail.com>
2021-08-17 17:35:35 +0100

    added avatar settings

--- controllers/settingsController.js
+++ controllers/settingsController.js
@@ -5,6 +5,30 @@ const Subscription = require("../models/subscription");
 const updatePasswordSchema = require("../validation/updatePasswordSchema");
 const bcrypt = require("bcryptjs");
 const createError = require("http-errors");
+const multer = require("multer");
+const fs = require("fs");
+
+const avatarStorage = multer.diskStorage({
+  destination: (req, res, cb) => {
+    cb(null, "uploads/avatars/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const avatarFilter = async (req, file, cb) => {
+  if (file.mimetype !== "image/jpeg" && file.mimetype !== "image/png") {
+    cb(new Error("Only jpg and png files are allowed"), false);
+    return;
+  }
+
+  cb(null, true);
+};
+const uploadAvatar = multer({
+  storage: avatarStorage,
+  fileFilter: avatarFilter,
+}).single("avatar");
 
 module.exports = {
   updateProfile: async (req, res, next) => {
@@ -55,5 +79,50 @@ module.exports = {
     } catch (err) {
       next(err);
     }
+  },
+
+  updateAvatar: async (req, res, next) => {
+    uploadAvatar(req, res, async err => {
+      try {
+        if (err) {
+          throw createError.UnprocessableEntity(err.message);
+        }
+
+        let user = await User.findById(req.user.id);
+
+        const avatarFullPathFolder = process.env.APP_URL + "/uploads/avatars/";
+        const newAvatar = avatarFullPathFolder + req.file.filename;
+        const oldAvatar = user.avatar;
+        const oldAvatarWithoutAppUrl = oldAvatar.split(process.env.APP_URL + "/")[1];
+
+        if (fs.existsSync(oldAvatarWithoutAppUrl)) {
+          fs.unlinkSync(oldAvatarWithoutAppUrl);
+        }
+
+        user.avatar = newAvatar;
+        await user.save();
+
+        user = user.toJSON();
+
+        const newAccessToken = await authService.createAccessToken(user);
+        const newRefreshToken = await authService.createRefreshToken(user);
+        authService.setRefreshTokenCookie(res, newRefreshToken);
+
+        user.subscriptions = await Subscription.find({ subscriber: user.id }).populate({
+          path: "subscribedTo",
+          populate: {
+            path: "subscribersCount",
+          },
+        });
+
+        return res.status(201).json({
+          accessToken: newAccessToken,
+          accessTokenEndDate: Date.now() + authService.accessTokenLifeTime,
+          user,
+        });
+      } catch (err) {
+        next(err);
+      }
+    });
   },
 };
--- routes/settings.js
+++ routes/settings.js
@@ -1,7 +1,12 @@
 const settingsRouter = require("express").Router();
-const { updateProfile, updatePassword } = require("../controllers/settingsController");
+const {
+  updateProfile,
+  updatePassword,
+  updateAvatar,
+} = require("../controllers/settingsController");
 
 settingsRouter.put("/profile", updateProfile);
 settingsRouter.put("/password", updatePassword);
+settingsRouter.put("/avatar", updateAvatar);
 
 module.exports = settingsRouter;
